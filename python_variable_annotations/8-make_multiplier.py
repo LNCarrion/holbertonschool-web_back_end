1c996a2d4a2c56fcb38bac743df46ce95ec2dc6c
LNCarrion <lestycarrion@gmail.com>
2024-04-25 11:44:26 -0400

    make multiplier

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,10 @@
+#!usr/bin/env python3
+"""making a multiplier"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """returns a function that multiplies floats"""
+    def multiplier_func(x: float) -> float:
+        return x * multiplier
+    return multiplier_func
