4da3b1011cc3b4cf8d61adeb3ecddf6902b0e17b
LNCarrion <lestycarrion@gmail.com>
2024-04-25 10:11:19 -0400

    file 5 adding a sum list

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""sum a list"""
+from typing import List
+
+
+def sum_list(input_list: list[float]) -> float:
+    """return the sum of float in a list"""
+    return sum(input_list)
