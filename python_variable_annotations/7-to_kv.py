56867cba3bf449c19fc97959e5c83f496535cb03
LNCarrion <lestycarrion@gmail.com>
2024-04-25 11:28:35 -0400

    fix pycode

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -3,6 +3,6 @@
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int,float]) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """return a tuple with the string k and the scquare of v as a float"""
     return k, float(v) ** 2
