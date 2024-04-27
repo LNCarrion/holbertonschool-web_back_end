07d422f27475bfaea76db98d74b7e70eb6314c8d
LNCarrion <lestycarrion@gmail.com>
2024-04-26 20:11:37 -0400

    async functions

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Async basics, tasks """
+
+from asyncio import Task, create_task
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+def task_wait_random(max_delay: int) -> Task:
+    """ Return a task """
+    return create_task(wait_random(max_delay))
