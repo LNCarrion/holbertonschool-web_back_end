1920c2988d62dbcb404e39b6fe16170e1a46df71
LNCarrion <lestycarrion@gmail.com>
2024-04-26 20:23:36 -0400

    fix file name and task 4

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
-""" Async basics, tasks """
+""" Async basics, random tasks """
 
-from asyncio import Task, create_task
+import asyncio
+from typing import List
+
+wait_random = __import__('3-tasks').wait_random
 
 
-wait_random = __import__('0-basic_async_syntax').wait_random
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """Returns list of completed random delay results for n tasks with a given max_delay """
 
+    tasks = [wait_random(max_delay) for _ in range(n)]
 
-def task_wait_random(max_delay: int) -> Task:
-    """ Return a task """
-    return create_task(wait_random(max_delay))
+    return [await task for task in asyncio.as_completed(tasks)]
