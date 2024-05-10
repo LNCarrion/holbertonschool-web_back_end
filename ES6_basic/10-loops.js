a7cb1b22201d6dbc70c9a58e1fc41a5167962f0c
LNCarrion <lestycarrion@gmail.com>
2024-05-10 11:29:23 -0400

    deliting a line

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,6 +1,5 @@
 export default function appendToEachArrayValue(array, appendString) {
     for (let [idx, value] of array.entries()) {
-      var value = array[idx];
       array[idx] = appendString + value;
     }
 
