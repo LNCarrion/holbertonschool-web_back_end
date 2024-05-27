d5cc0d6258ba8b157968f22eea9abefae790dd7e
LNCarrion <lestycarrion@gmail.com>
2024-05-27 12:08:19 -0400

    fix code 10

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -3,9 +3,9 @@ export default function updateUniqueItems(map) {
     throw new Error('cannot process');
   }
 
-  for (const [key, value] of map) {
+  map.forEach ((value, key) => {
     if (value === 1) {
       map.set(key, 100);
     }
-  }
+  });
 }
